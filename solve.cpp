84d0abbf24bc48ea0d58d02feeba72ba77e2fe96
gulyonatyoma <artyomgostev26082005@gmail.com>
2023-10-14 00:35:09 +0300

    change

--- solve.cpp
+++ solve.cpp
@@ -1,6 +1,17 @@
-#include "sum.h"
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    cout << sum(-1,3);
+#define ll long long
+void solve(){
+
+}
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+        cout << '\n';
+    }
+    return 0;
 }
