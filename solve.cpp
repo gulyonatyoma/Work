7139244ef3e63abac5a2dcde9ad50a8c978ee066
gulyonatyoma <artyomgostev26082005@gmail.com>
2023-10-18 01:57:50 +0300

    solve

--- solve.cpp
+++ solve.cpp
@@ -1,11 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-void solve(){
+void slv(){
+    queue<ll> q;
     ll n; cin >> n;
-    vector<ll> a(n);
-    for (auto& it : a) cin >> it;
-    for (auto& it : a) cout << it << ' ';
+    for (ll i = 0; i < n; i++){
+        char c; cin >> c;
+        if (c == '-'){
+            cout << q.front() << '\n';
+            q.pop();
+        } else if (c == '+'){
+            ll x; cin >> x;
+            q.push(x);
+        } else {
+            ll x; cin >> x;
+            queue<ll> p;
+            for (ll i = 0; i < q.size() / 2; i++) p.push(q.front()), q.pop();
+            p.push(x);
+            for (ll i = q.size() / 2; i < q.size(); i++) p.push(q.front()), q.pop();
+            q = p;
+        }
+    }
 }
 signed main(){
     ios::sync_with_stdio(false);
@@ -13,7 +28,7 @@ signed main(){
     ll t = 1;
     //cin >> t;
     while(t--){
-        solve();
+        slv();
         cout << '\n';
     }
     return 0;
